2f33ab871545e7271c46565feb39a8ddf4332e4d
Lucas Wenck <lucas.wenck@gmail.com>
2023-03-13 23:13:16 -0300

    Finish method that does the cipher

--- Caesar-Cipher.rb
+++ Caesar-Cipher.rb
@@ -3,4 +3,19 @@ def Caesar_Cipher (changed = '')
     string = gets.chomp
     puts "Whats the shift?"
     shift = gets.chomp
+    shift = shift.to_i
+
+    string.each_char do |char|
+        baseline = char.ord < 91 ? 65 : 97
+        if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+            change = (((char.ord - baseline) + shift) % 26) + baseline
+            changed += change.chr
+        else
+            changed += char
+        end
+    end
+    puts changed
+    changed
 end
+
+Caesar_Cipher()
